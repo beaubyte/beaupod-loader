df4ff27b89cfa848c55f96c8263307d58a3b711d
Beau <beau@mochabeau.red>
2026-01-15 13:37:49 -0600

    first version

---
+++ app.py
@@ -0,0 +1,12 @@
+import requests
+
+# need to still do api key stuff
+API_KEY = ''
+USERNAME = ''
+
+def main():
+    api_endpoint = 'http://ws.audioscrobbler.com/2.0/'
+
+    parameters = {
+        ''
+    }
